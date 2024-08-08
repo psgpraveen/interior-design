76301eddca5689683b5044819a83897ea4c675e7
Praveen Kumar Gupta <pg23403@gmail.com>
2024-08-08 16:24:27 +0530

    Few update

--- src/Component/Card/Index.js
+++ src/Component/Card/Index.js
@@ -4,7 +4,7 @@ import axios from 'axios';
 const Index = () => {
   const [products, setProducts] = useState([]);
   const [error, setError] = useState(null); // Added state for error handling
- const url =process.env.FetchUrl || 'http://localhost:5000/';
+  const url = process.env.REACT_APP_FETCH_URL ? `${process.env.REACT_APP_FETCH_URL}` : "http://localhost:5000";  
   const fetchProducts = async () => {
     try {
       const response = await axios.get(url);
@@ -44,19 +44,7 @@ const Index = () => {
               <p className="text-gray-600 mt-2">{product.Description}</p>
               <div className="mt-4 flex items-center flex-wrap gap-2">
                 <h4 className="text-lg font-bold text-gray-800">${product.Price}</h4>
-                <div className="bg-gray-100 w-10 h-10 flex items-center justify-center rounded-full cursor-pointer ml-auto">
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    width="16px"
-                    className="fill-gray-800 inline-block"
-                    viewBox="0 0 64 64"
-                  >
-                    <path
-                      d="M45.5 4A18.53 18.53 0 0 0 32 9.86 18.5 18.5 0 0 0 0 22.5C0 40.92 29.71 59 31 59.71a2 2 0 0 0 2.06 0C34.29 59 64 40.92 64 22.5A18.52 18.52 0 0 0 45.5 4ZM32 55.64C26.83 52.34 4 36.92 4 22.5a14.5 14.5 0 0 1 26.36-8.33 2 2 0 0 0 3.27 0A14.5 14.5 0 0 1 60 22.5c0 14.41-22.83 29.83-28 33.14Z"
-                      data-original="#000000"
-                    ></path>
-                  </svg>
-                </div>
+                
               </div>
             </div>
           </div>
