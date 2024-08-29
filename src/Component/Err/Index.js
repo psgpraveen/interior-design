605b23052053582015085f5baf48e297a5d67442
Praveen Kumar Gupta <pg23403@gmail.com>
2024-08-30 00:14:52 +0530

    Few update

--- src/Component/Err/Index.js
+++ src/Component/Err/Index.js
@@ -11,7 +11,7 @@ const Index = () => {
   
           <p className="mb-8 text-center text-gray-500 sm:text-left md:text-lg">The page you’re looking for doesn’t exist.</p>
   
-          <Link to="/interior-design" className="inline-block rounded-lg bg-gray-200 px-8 py-3 text-center text-sm font-semibold text-gray-500 outline-none ring-indigo-300 transition duration-100 hover:bg-gray-300 focus-visible:ring active:text-gray-700 md:text-base">Go home</Link>
+          <Link to="/" className="inline-block rounded-lg bg-gray-200 px-8 py-3 text-center text-sm font-semibold text-gray-500 outline-none ring-indigo-300 transition duration-100 hover:bg-gray-300 focus-visible:ring active:text-gray-700 md:text-base">Go home</Link>
         </div>
   
         <div className="relative h-80 overflow-hidden rounded-lg bg-gray-100 shadow-lg md:h-auto">
