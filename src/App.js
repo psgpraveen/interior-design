16d0b4c3b4d4533bc5c4491c82fb58b708d27da6
Praveen Kumar Gupta <62584529+psgpraveen@users.noreply.github.com>
2024-10-23 06:19:25 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -22,7 +22,7 @@ function App() {
   return (
     <Router><Analytics/>
       <Routes>
-        <Route path="/psg" element={
+        <Route path="/" element={
           <>
             <Header />
             <Intro />
